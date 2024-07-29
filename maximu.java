6e209588670d3f34a673ef00e080cbfb5faacdf3
kavin108 <145475464+kavin108@users.noreply.github.com>
2024-07-29 16:32:28 +0530

    max number

---
+++ maximu.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class maximu {
+
+	public static void main(String[] args) {
+	int[]a= {47,36,27,28};
+	int min=a[0];
+	for(int i =1;i<a.length;i++) {
+		if(a[i]<min)
+min=a[i];
+	}
+	System.out.println();
+
+	}
+
+}
